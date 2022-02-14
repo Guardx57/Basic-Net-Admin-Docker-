9b3eb5f5389ad4bd8d2b7c9f922cfdfc6cfac6e7
Guardx57 <dhia.muafa@binus.ac.id>
2022-02-14 11:55:02 -0800

    add new php

--- index.php
+++ index.php
@@ -10,13 +10,14 @@ mysqli_select_db($conn,"Trucorp");
 $result = mysqli_query($conn,"SELECT * FROM users");
 
 echo "<table style='border: 1px solid black;'>";
+$jumlah=0
 while($row = mysqli_fetch_array($result)){
-
+$jumlah+=1
 echo "<tr><td style='border: 1px solid black;'>" . htmlspecialchars($row['ID']) . "</td><td style='border: 1px solid black;'>" . htmlspecialchars($row['Nama']) . "</td><td style='border: 1px solid black;'>" . htmlspecialchars($row['Alamat']) . "</td><td style='border: 1px solid black;'>" . htmlspecialchars($row['Jabatan']) . "</td></tr>";  
 }
 
 echo "</table>"; 
-
+echo "Jumlah User pada Database: " . $jumlah;
 mysqli_close($conn); 
 
 ?>
